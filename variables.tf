9e208115ea0b5abefb3b00d88d4b91984071dad3
vishwaspg06 <vishwaspg06@gmail.com>
2026-04-20 20:50:10 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
+}
+
+variable "public_key_name" {
+  type        = string
+  description = "Name of an existing AWS key pair for SSH access"
+  default     = "323230574799_NV-Aug-24"
+}
